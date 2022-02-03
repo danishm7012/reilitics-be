b6048cda866c7483392feb664d9e76066da11c49
danishm7012 <danishm7012@gmail.com>
2022-02-03 18:09:02 +0500

    json converted

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -1,5 +1,5 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 import {
   Test,
   createResource,
@@ -7,14 +7,16 @@ import {
   updateResource,
   getResourceById,
   deleteResource,
-} from '../controllers/statsController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
+  getRentalJson,
+} from "../controllers/statsController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route('/test').get(Test).post(protect, admin, createResource)
+router.route("/test").get(Test).post(protect, admin, createResource);
+router.route("/rentaljson").get(getRentalJson).post(protect, admin, createResource);
 router
-  .route('/:id')
+  .route("/:id")
   .put(protect, admin, updateResource)
   .get(getResourceById)
-  .delete(protect, admin, deleteResource)
+  .delete(protect, admin, deleteResource);
 
-export default router
+export default router;
