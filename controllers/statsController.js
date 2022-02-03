8be51396bd08e4cd6a74f8241cef7990a2e4cecb
Danish Mehmood <danishm7012@gmail.com>
2022-02-03 21:18:45 +0500

    fixed rental

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,35 +1,32 @@
-import asyncHandler from "express-async-handler";
-import Resource from "../models/resourceModel.js";
-import fs from "fs";
-import https from "https";
-import path from "path";
-import { rentalJson } from "../data/json/rental.js";
+import asyncHandler from 'express-async-handler'
+import Resource from '../models/resourceModel.js'
+import fs from 'fs'
+import https from 'https'
+import path from 'path'
+import { rentalJson } from '../data/json/rental.js'
 //import CSV from 'csvtojson'
 
 // @desc    Fetch single resource
 // @route   GET /api/stats/test
 // @access  Public
 const Test = asyncHandler(async (req, res) => {
-  res.json({ success: true, code: 200, message: "Test state" });
-});
+  res.json({ success: true, code: 200, message: 'Test state' })
+})
 
 // @desc    Fetch all resources
 // @route   GET /api/resource
 // @access  Public
 const getRentalJson = asyncHandler(async (req, res) => {
-  const pageSize = 10;
-  const page = Number(req.query.pageNumber) || 1;
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1
   const rentaljson = await rentalJson.map((item) => ({
     regionName: item.RegionName,
-    "2018-01": item["2018-01"],
-    "2018-12": item["2018-12"],
-    "2019-01": item["2019-01"],
-    "2019-12": item["2019-12"],
-    "2020-01": item["2020-01"],
-    "2020-12": item["2020-12"],
-    "2021-01": item["2021-01"],
-    "2021-12": item["2021-12"],
-  }));
+    2018: ((item['2018-12'] - item['2018-01']) / item['2018-12']) * 100,
+    2019: ((item['2019-12'] - item['2019-01']) / item['2019-12']) * 100,
+    2020: ((item['2020-12'] - item['2020-01']) / item['2020-12']) * 100,
+    2021: ((item['2021-12'] - item['2021-01']) / item['2021-12']) * 100,
+    // 2022: ((item['2022-12'] - item['2022-01']) / item['2022-12']) * 100,
+  }))
 
   // const count = await Resource.countDocuments();
   // const rentaljson = await Resource.find({})
@@ -42,35 +39,35 @@ const getRentalJson = asyncHandler(async (req, res) => {
     rentaljson,
     // page,
     // pages: Math.ceil(count / pageSize),
-  });
-});
+  })
+})
 // @desc    Fetch single resource
 // @route   GET /api/stats/test
 // @access  Public
 const test = asyncHandler(async (req, res) => {
-  res.json({ success: true, code: 200, message: "Test succerss" });
-});
+  res.json({ success: true, code: 200, message: 'Test succerss' })
+})
 
 // @desc    Fetch all resources
 // @route   GET /api/resource
 // @access  Public
 const getResources = asyncHandler(async (req, res) => {
-  const pageSize = 10;
-  const page = Number(req.query.pageNumber) || 1;
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword
     ? {
         name: {
           $regex: req.query.keyword,
-          $options: "i",
+          $options: 'i',
         },
       }
-    : {};
+    : {}
 
-  const count = await Resource.countDocuments({ ...keyword });
+  const count = await Resource.countDocuments({ ...keyword })
   const resources = await Resource.find({ ...keyword })
     .limit(pageSize)
-    .skip(pageSize * (page - 1));
+    .skip(pageSize * (page - 1))
 
   res.json({
     success: true,
@@ -78,112 +75,112 @@ const getResources = asyncHandler(async (req, res) => {
     resources,
     page,
     pages: Math.ceil(count / pageSize),
-  });
-});
+  })
+})
 
 // @desc    Fetch single resource
 // @route   GET /api/resource/:id
 // @access  Public
 const getResourceById = asyncHandler(async (req, res) => {
-  const resourceFound = await Resource.findById(req.params.id);
+  const resourceFound = await Resource.findById(req.params.id)
 
   if (resourceFound) {
-    res.json({ success: true, code: 200, resourceFound });
+    res.json({ success: true, code: 200, resourceFound })
   } else {
-    res.status(404);
-    throw new Error("resource not found");
+    res.status(404)
+    throw new Error('resource not found')
   }
-});
+})
 
 // @desc    Delete a resource
 // @route   DELETE /api/resource/:id
 // @access  Private/Admin
 const deleteResource = asyncHandler(async (req, res) => {
-  const resourceFound = await Resource.findById(req.params.id);
+  const resourceFound = await Resource.findById(req.params.id)
 
   if (resourceFound) {
-    await resourceFound.remove();
+    await resourceFound.remove()
     res.json({
       success: true,
-      code: "200",
-      message: "Resource removed successfully!",
-    });
+      code: '200',
+      message: 'Resource removed successfully!',
+    })
   } else {
-    res.status(404);
-    throw new Error("Resource not found!");
+    res.status(404)
+    throw new Error('Resource not found!')
   }
-});
+})
 
 // @desc    Create a resource
 // @route   POST /api/resource
 // @access  Private
 const createResource = asyncHandler(async (req, res) => {
-  const { title, resourceType, resourceUrl, imageUrl } = req.body;
+  const { title, resourceType, resourceUrl, imageUrl } = req.body
   const resourceData = new Resource({
     AddedByAdmin: req.user.id,
     title,
     resourceType,
     imageUrl,
     resourceUrl,
-  });
+  })
 
-  const createdResource = await resourceData.save();
+  const createdResource = await resourceData.save()
   res.status(201).json({
     success: true,
     code: 200,
-    message: "Resource created successfully!",
+    message: 'Resource created successfully!',
     createdResource,
-  });
-});
+  })
+})
 
 // @desc    Update a resource
 // @route   PUT /api/resource/:id
 // @access  Private/Admin
 const updateResource = asyncHandler(async (req, res) => {
-  const { title, resourceType, resourceUrl, imageUrl } = req.body;
+  const { title, resourceType, resourceUrl, imageUrl } = req.body
 
-  const resourceFound = await Resource.findById(req.params.id);
+  const resourceFound = await Resource.findById(req.params.id)
 
   if (resourceFound) {
-    resourceFound.title = title || resourceFound.title;
-    resourceFound.resourceType = resourceType || resourceFound.resourceType;
-    resourceFound.resourceUrl = resourceUrl || resourceFound.resourceUrl;
-    resourceFound.imageUrl = imageUrl || resourceFound.imageUrl;
+    resourceFound.title = title || resourceFound.title
+    resourceFound.resourceType = resourceType || resourceFound.resourceType
+    resourceFound.resourceUrl = resourceUrl || resourceFound.resourceUrl
+    resourceFound.imageUrl = imageUrl || resourceFound.imageUrl
 
-    const updatedResource = await resourceFound.save();
+    const updatedResource = await resourceFound.save()
     res.json({
       success: true,
       code: 200,
-      message: "Resource updated successfully!",
+      message: 'Resource updated successfully!',
       updatedResource,
-    });
+    })
   } else {
-    res.status(404);
-    throw new Error("Resource not found");
+    res.status(404)
+    throw new Error('Resource not found')
   }
-});
+})
 // @desc    Update a resource status
 // @route   PUT /api/resource/status/:id
 // @access  Private
 const updateResourceStatus = asyncHandler(async (req, res) => {
-  const { status } = req.body;
-  const resourceFound = await Resource.findById(req.params.id);
-  console.log(resourceFound.status);
+  const { status } = req.body
+  const resourceFound = await Resource.findById(req.params.id)
+  console.log(resourceFound.status)
   if (resourceFound) {
-    resourceFound.status = status;
+    resourceFound.status = status
 
-    const updatedResource = await resourceFound.save();
+    const updatedResource = await resourceFound.save()
     res.json({
       success: true,
       code: 200,
-      message: "Resource status updated successfully!",
+      message: 'Resource status updated successfully!',
       updatedResource,
-    });
+    })
   } else {
-    res.status(404);
-    throw new Error("Resource not found");
+    res.status(404)
+    throw new Error('Resource not found')
   }
-});
+})
 
 export {
   Test,
@@ -194,4 +191,4 @@ export {
   updateResource,
   getRentalJson,
   updateResourceStatus,
-};
+}
