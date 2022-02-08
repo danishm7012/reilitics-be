461037b6e3d2a0429cbe007907aadb98308df868
danishm7012 <danishm7012@gmail.com>
2022-02-08 16:47:07 +0500

    pagination in newsletter"

--- controllers/newsLetterController.js
+++ controllers/newsLetterController.js
@@ -1,46 +1,51 @@
-import asyncHander from 'express-async-handler'
-import NewsLetter from '../models/newsLetterModel.js';
-import { validatenewletter } from '../validation/newLetterValidation.js';
+import asyncHander from "express-async-handler";
+import NewsLetter from "../models/newsLetterModel.js";
+import { validatenewletter } from "../validation/newLetterValidation.js";
 
+const createNewsletter = asyncHander(async (req, res) => {
+  const { isValid, errors } = await validatenewletter(req.body);
+  if (!isValid) {
+    res.status(403).json({
+      success: false,
+      code: 403,
+      message: errors,
+    });
+  }
+  const { email } = req.body;
 
+  const emailExists = await NewsLetter.findOne({ email });
 
-const createNewsletter = asyncHander(async(req,res)=>{
-    const {isValid, errors} = await validatenewletter(req.body);
-    if(!isValid){
-        res.status(403).json({
-            success: false,
-            code: 403,
-            message: errors
-        })
-    }
-    const {email } = req.body;
+  if (emailExists) {
+    res.status(400);
+    throw new Error("This Email already exists");
+  }
+  const news = new NewsLetter({
+    email,
+  });
+  const createNewsletter = await news.save();
+  res.status(201).json({ success: true, code: 200, createNewsletter });
+});
 
-    const emailExists = await NewsLetter.findOne({email})
+const getNewsLetter = asyncHander(async (req, res) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
 
-    if(emailExists){
-        res.status(400);
-            throw new Error("This Email already exists")
-    }
-    const news = new NewsLetter({
-        email
-    })
-    const createNewsletter = await news.save();
-    res.status(201).json({ success: true, code: 200, createNewsletter });
-}) 
+  const count = await NewsLetter.countDocuments();
+  const newsletters = await NewsLetter.find()
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
 
-const getNewsLetter = asyncHander(async(req,res) =>{
+  if (newsletters) {
+    res.json({
+      success: true,
+      code: 200,
+      newsletters,
+      page,
+      pages: Math.ceil(count / pageSize),
+    });
+  } else {
+    throw new Error("NewsLetters not found");
+  }
+});
 
-    const news =  await NewsLetter.find();
-
-    if(news)
-    {
-        res.json({success: true, code: 200, news})
-    }
-    else{
-        throw new Error("NewsLetter not found")
-    }
-
-
-})
-
-export {getNewsLetter, createNewsletter}
+export { getNewsLetter, createNewsletter };
--- routes/newLetterRoutes.js
+++ routes/newLetterRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const router = express.Router();
 
+
 import {getNewsLetter, createNewsletter} from '../controllers/newsLetterController.js'
 
 router.route("/").post(createNewsletter).get(getNewsLetter)
