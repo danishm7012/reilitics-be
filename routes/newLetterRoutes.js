fcac8ceb72435a0ded4e8a65304cfa8a213f7d6a
Ahmad129096 <72014036+Ahmad129096@users.noreply.github.com>
2022-02-08 17:01:04 +0500

    merging

--- routes/newLetterRoutes.js
+++ routes/newLetterRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 import {getNewsLetter, createNewsletter} from '../controllers/newsLetterController.js'
 import {protect , admin} from '../middleware/authMiddleware.js'
 
-router.route("/").post(createNewsletter).get(protect, admin , getNewsLetter)
+router.route("/").post(createNewsletter).get( getNewsLetter)
 
 export default router
