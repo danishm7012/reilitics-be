f17b19a62fb30ee39d6bc35f05c4240cbafe376b
Danish Mehmood <danishm7012@gmail.com>
2022-02-21 23:08:20 +0500

    fix merging

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -11,16 +11,9 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(protect, getNotes).post(protect, createNote)
 router
-<<<<<<< HEAD
-  .route("/:id")
-  .put(protect, updateNote)
-  .get(getNoteById)
-  .delete(protect, deleteNote);
-=======
   .route('/:id')
   .put(protect, updateNote)
   .get(getNoteById)
   .delete(protect, deleteNote)
->>>>>>> 510a1187e9a42fe74c8ff17987a509fc3bb820d3
 
 export default router
