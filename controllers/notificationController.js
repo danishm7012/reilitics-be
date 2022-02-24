ab43b64eb521f03e0dd307262951653c188adff7
Ahmad129096 <72014036+Ahmad129096@users.noreply.github.com>
2022-02-24 12:31:05 +0500

    get notification

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,45 +1,40 @@
-import asyncHandler from 'express-async-handler'
-import Notification from '../models/notificationModel.js';
-import Package from '../models/packageModel.js';
-import User from '../models/userModel.js'
+import asyncHandler from "express-async-handler";
+import Notification from "../models/notificationModel.js";
+import Package from "../models/packageModel.js";
+import User from "../models/userModel.js";
 
-const createNotification =asyncHandler(async(req,res)=>{
-    const {subject, description, userType} = req.body;
+const createNotification = asyncHandler(async (req, res) => {
+  const { subject, description, userType } = req.body;
 
-    const notification = new Notification({
-        subject,
-        description,
-        userType,
-        user: req.user._id
-    })
+  const notification = new Notification({
+    subject,
+    description,
+    userType,
+    user: req.user._id,
+  });
 
-    const create = await notification.save();
-    res.status(201).json({
-        success: true,
-        code: 200,
-        message: "Notification has been genertaed",
-        create
-    })
-})
+  const create = await notification.save();
+  res.status(201).json({
+    success: true,
+    code: 200,
+    message: "Notification has been genertaed",
+    create,
+  });
+});
 
-const getNotification = asyncHandler(async(req,res)=>{
-
-    const notification = await Notification.find({user: req.user._id})
-    const user = await User.find({})
-    
-    if( user[0].packageID === notification[0].userType)  
-    {
-        console.log(user[0].packageID)
-        console.log(notification[0].userType)
-        console.log("packge id from notification",notification)
+const getNotification = asyncHandler(async (req, res) => {
+  const notification = await Notification.find({ user: req.user._id });
+  const user = await User.find({});
+  Object.keys(notification).forEach(function (key) {
+   if (notification[key].userType === user[key].packageID)
+   {
+        res.json(notification[key])
     }
-    else{
-        console.log("no result found")
-    }
-    // res.json(notification)
-    
-    // console.log(pkg)
+  });
+ 
+  console.log("No notification found")
+});
 
-})
 
-export {createNotification, getNotification}
+
+export { createNotification, getNotification };
