1ccc042321aa602646b3455ce7ae358c660f2454
Danish Mehmood <danishm7012@gmail.com>
2022-03-16 19:57:41 +0500

    add educatinal attainment and population by race APIs

--- controllers/demographicStats.js
+++ controllers/demographicStats.js
@@ -1,76 +1,104 @@
-import asyncHandler from "express-async-handler";
-import CSV from "csvtojson";
-import _ from "lodash";
+import asyncHandler from 'express-async-handler'
+import CSV from 'csvtojson'
+import _ from 'lodash'
 
 // @desc    Test demographic Stats controllat
 // @route   GET /api/demographic/test
 // @access  Public
 const Test = asyncHandler(async (req, res) => {
-  res.json({ success: true, code: 200, message: "Test Demographic." });
-});
+  res.json({ success: true, code: 200, message: 'Test Demographic.' })
+})
 
 // @desc    Fetch population
 // @route   GET /api/demographic/population
 // @access  Public
 const population = asyncHandler(async (req, res) => {
-  const { Region } = req.body;
-  let Data;
+  const { Region } = req.body
+  let Data
   const populationJson = await CSV().fromFile(
-    "./data/demographic/populationData.csv"
-  );
+    './data/demographic/population.csv'
+  )
 
   const population = await _.omit(
     populationJson.find((item) => {
-      return item.Region == Region;
+      return item.Region == Region
     }),
-    ["Region", "Parent code", "Country code"]
-  );
+    ['Region']
+  )
   res.json({
     success: true,
     code: 200,
     message: `Population of ${Region}.`,
     Data: population,
-  });
-});
-// @desc    Fetch Educational and race data
-// @route   GET /api/demographic/educational_and_race
+  })
+})
+// @desc    Fetch Educational attainment
+// @route   GET /api/demographic/educational_attainment
 // @access  Public
-const EducationalOrRace = asyncHandler(async (req, res) => {
-  const { Region } = req.body;
-  let Data;
-  const educational_race_json = await CSV().fromFile(
-    "./data/demographic/Educational_and_race.csv"
-  );
+const educationalAttainment = asyncHandler(async (req, res) => {
+  const { Region } = req.body
 
-  // const population = await _.omit(
-  //   populationJson.find((item) => {
-  //     return item.Region == Region;
-  //   }),
-  //   ["Region", "Parent code", "Country code"]
-  // );
+  const educational_attainment_json = await CSV().fromFile(
+    './data/demographic/Education_attainment.csv'
+  )
+
+  const Result = educational_attainment_json.filter((item) =>
+    item.Region.includes(`${Region}`)
+  )
+
   res.json({
     success: true,
     code: 200,
-    message: `Educational_and_race`,
-    Data: educational_race_json,
-  });
-});
+    message: `Educational attainment.`,
+    Data: Result,
+  })
+})
+// @desc    Fetch Population by race
+// @route   GET /api/demographic/population_by_race
+// @access  Public
+const populationByRace = asyncHandler(async (req, res) => {
+  const { Region } = req.body
+
+  const population_by_race = await CSV().fromFile(
+    './data/demographic/population_by_race.csv'
+  )
+
+  const Data = await _.omit(
+    population_by_race.find((item) => {
+      return item.Region == Region
+    }),
+    ['Region']
+  )
+
+  res.json({
+    success: true,
+    code: 200,
+    message: `Population of ${Region} by race.`,
+    Data,
+  })
+})
 
 // @desc    Fetch regions
 // @route   GET /api/demographic/regions
 // @access  Public
 const demographicRegions = asyncHandler(async (req, res) => {
   const populationJson = await CSV().fromFile(
-    "./data/demographic/populationData.csv"
-  );
-  let Data = populationJson.map((item) => item.Region);
+    './data/demographic/population.csv'
+  )
+  let Data = populationJson.map((item) => item.Region)
 
   res.json({
     success: true,
     code: 200,
     message: `Demographic regions.`,
     Data,
-  });
-});
+  })
+})
 
-export { Test, demographicRegions, population, EducationalOrRace };
+export {
+  Test,
+  demographicRegions,
+  population,
+  educationalAttainment,
+  populationByRace,
+}
