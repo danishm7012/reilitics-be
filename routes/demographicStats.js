4d8052ccdf7403a66e20bcad41e7edc0984062a7
Danish Mehmood <danishm7012@gmail.com>
2022-03-17 23:23:58 +0500

    graph completed

--- routes/demographicStats.js
+++ routes/demographicStats.js
@@ -4,12 +4,20 @@ import {
   demographicRegions,
   educationalAttainment,
   population,
+  populationByAge,
   populationByRace,
+  renterVsOwner,
+  incomeByHouseholdType,
+  householdByType,
   Test,
 } from '../controllers/demographicStats.js'
 
 router.route('/test').get(Test)
 router.route('/population').post(population)
+router.route('/population_by_age').post(populationByAge)
+router.route('/renterVsOwner').post(renterVsOwner)
+router.route('/income_by_household_type').post(incomeByHouseholdType)
+router.route('/household_by_type').post(householdByType)
 router.route('/educational_attainment').post(educationalAttainment)
 router.route('/population_by_race').post(populationByRace)
 router.route('/regions').get(demographicRegions)
