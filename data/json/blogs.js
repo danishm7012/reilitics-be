59470aabefb2c8887522b3a5938d87d28e64997a
Danish Mehmood <danishm7012@gmail.com>
2022-04-25 13:16:38 +0500

    deployemnt

---
+++ data/json/blogs.js
@@ -0,0 +1,194 @@
+const blogdetail = [
+  {
+    title: "1 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_2",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "2 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Realstate",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "3 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_2",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "4 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Realstate",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "5 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_2",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "6 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Realstate",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "7 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_2",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "8 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Realstate",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "9 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_2",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "10 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Realstate",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "11 which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_2",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_1",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_1",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_1",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_1",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_1",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_1",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Uncategory_1",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "which imac i should buy?",
+    user: "620f421e8a7b15fb88751224",
+    author: "Mr. Danish",
+    category: "Realstate",
+    image:
+      "https://reilitics-be.herokuapp.com/uploads\\image-1645169786363.jpg",
+    detail:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+];
+
+export { blogdetail };
